2ea019e4eb4b1fbb3c6368965bd76dd5892b3e70
Bojan Radojevic <bradojevic@gmail.com>
2016-02-18 16:40:58 +0100

    Backmerge from history.

--- 0.9.2.1/run.sh
+++ 0.9.2.1/run.sh
@@ -5,20 +5,6 @@ CONFIG_FILE="/config/config.toml"
 INFLUX_HOST="localhost"
 INFLUX_API_PORT="8086"
 API_URL="http://${INFLUX_HOST}:${INFLUX_API_PORT}"
-ADMIN=${ADMIN_USER:-root}
-PASS=${INFLUXDB_INIT_PWD:-root}
-
-wait_for_start_of_influxdb(){
-    #wait for the startup of influxdb
-    RET=1
-    while [[ RET -ne 0 ]]; do
-        echo "=> Waiting for confirmation of InfluxDB service startup ..."
-        sleep 3
-        curl -k ${API_URL}/ping 2> /dev/null
-        RET=$?
-    done
-}
-
 
 # Dynamically change the value of 'max-open-shards' to what 'ulimit -n' returns
 sed -i "s/^max-open-shards.*/max-open-shards = $(ulimit -n)/" ${CONFIG_FILE}
@@ -28,9 +14,9 @@ if [ -n "${FORCE_HOSTNAME}" ]; then
     if [ "${FORCE_HOSTNAME}" == "auto" ]; then
         #set hostname with IPv4 eth0
         HOSTIPNAME=$(ip a show dev eth0 | grep inet | grep eth0 | sed -e 's/^.*inet.//g' -e 's/\/.*$//g')
-        /usr/bin/perl -p -i -e "s/hostname = \"localhost\"/hostname = \"${HOSTIPNAME}\"/g" ${CONFIG_FILE}
+        /usr/bin/perl -p -i -e "s/^# hostname.*$/hostname = \"${HOSTIPNAME}\"/g" ${CONFIG_FILE}
     else
-        /usr/bin/perl -p -i -e "s/hostname = \"localhost\"/hostname = \"${FORCE_HOSTNAME}\"/g" ${CONFIG_FILE}
+        /usr/bin/perl -p -i -e "s/^# hostname.*$/hostname = \"${FORCE_HOSTNAME}\"/g" ${CONFIG_FILE}
     fi
 fi
 
@@ -79,84 +65,61 @@ if [ -n "${GRAPHITE_TEMPLATE}" ]; then
     sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/instance\.profile\.measurement\*/${GRAPHITE_TEMPLATE}/; }" ${CONFIG_FILE}
 fi
 
-# Add Collectd support
-if [ -n "${COLLECTD_DB}" ]; then
-    echo "COLLECTD_DB: ${COLLECTD_DB}"
-    sed -i -r -e "/^\[collectd\]/, /^$/ { s/false/true/; s/( *)# *(.*)\"collectd\"/\1\2\"${COLLECTD_DB}\"/g;}" ${CONFIG_FILE}
-fi
-if [ -n "${COLLECTD_BINDING}" ]; then
-    echo "COLLECTD_BINDING: ${COLLECTD_BINDING}"
-    sed -i -r -e "/^\[collectd\]/, /^$/ { s/( *)# *(.*)\":25826\"/\1\2\"${COLLECTD_BINDING}\"/g;}" ${CONFIG_FILE}
-fi
-if [ -n "${COLLECTD_RETENTION_POLICY}" ]; then
-    echo "COLLECTD_RETENTION_POLICY: ${COLLECTD_RETENTION_POLICY}"
-    sed -i -r -e "/^\[collectd\]/, /^$/ { s/( *)# *(retention-policy.*)\"\"/\1\2\"${COLLECTD_RETENTION_POLICY}\"/g;}" ${CONFIG_FILE}
-fi
-
 # Add UDP support
 if [ -n "${UDP_DB}" ]; then
-    sed -i -r -e "/^\[\[udp\]\]/, /^$/ { s/false/true/; s/#//g; s/\"udpdb\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
+    sed -i -r -e "/^\[udp\]/, /^$/ { s/false/true/; s/#//g; s/\"udpdb\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
 fi
 if [ -n "${UDP_PORT}" ]; then
-    sed -i -r -e "/^\[\[udp\]\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
+    sed -i -r -e "/^\[udp\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
 fi
 
 
-if [ -f "/data/.init_script_executed" ]; then
-    echo "=> The initialization script had been executed before, skipping ..."
-else
-    echo "=> Starting InfluxDB in background ..."
-    if [ -n "${JOIN}" ]; then
-        exec influxd -config=${CONFIG_FILE} -join ${JOIN} &
+echo "influxdb configuration: "
+cat ${CONFIG_FILE}
+echo "=> Starting InfluxDB ..."
+exec /opt/influxdb/influxd -config=${CONFIG_FILE} &
+
+# Pre create database on the initiation of the container
+if [ -n "${PRE_CREATE_DB}" ]; then
+    echo "=> About to create the following database: ${PRE_CREATE_DB}"
+    if [ -f "/data/.pre_db_created" ]; then
+        echo "=> Database had been created before, skipping ..."
     else
-        exec influxd -config=${CONFIG_FILE} &
-    fi
-
-    wait_for_start_of_influxdb
-
-    #Create the admin user
-    if [ -n "${ADMIN_USER}" ] || [ -n "${INFLUXDB_INIT_PWD}" ]; then
-        echo "=> Creating admin user"
-        influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="CREATE USER ${ADMIN} WITH PASSWORD '${PASS}' WITH ALL PRIVILEGES"
-    fi
-
-    # Pre create database on the initiation of the container
-    if [ -n "${PRE_CREATE_DB}" ]; then
-        echo "=> About to create the following database: ${PRE_CREATE_DB}"
         arr=$(echo ${PRE_CREATE_DB} | tr ";" "\n")
 
-        for x in $arr
-        do
-            echo "=> Creating database: ${x}"
-            echo "CREATE DATABASE ${x}" >> /tmp/init_script.influxql
+        #wait for the startup of influxdb
+        RET=1
+        while [[ RET -ne 0 ]]; do
+            echo "=> Waiting for confirmation of InfluxDB service startup ..."
+            sleep 3
+            curl -k ${API_URL}/ping 2> /dev/null
+            RET=$?
         done
+        echo ""
+
+        PASS=${INFLUXDB_INIT_PWD:-root}
+        if [ -n "${ADMIN_USER}" ]; then
+          echo "=> Creating admin user"
+          /opt/influxdb/influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="CREATE USER ${ADMIN_USER} WITH PASSWORD '${PASS}' WITH ALL PRIVILEGES"
+          for x in $arr
+          do
+              echo "=> Creating database: ${x}"
+              /opt/influxdb/influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN_USER} -password="${PASS}" -execute="create database ${x}"
+              /opt/influxdb/influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN_USER} -password="${PASS}" -execute="grant all PRIVILEGES on ${x} to ${ADMIN_USER}"
+          done
+          echo ""
+        else
+          for x in $arr
+          do
+              echo "=> Creating database: ${x}"
+              /opt/influxdb/influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -execute="create database \"${x}\""
+          done
+        fi
+
+        touch "/data/.pre_db_created"
     fi
-
-    # Execute influxql queries contained inside /init_script.influxql
-    if [ -f "/init_script.influxql" ] || [ -f "/tmp/init_script.influxql" ]; then
-        echo "=> About to execute the initialization script"
-
-        cat /init_script.influxql >> /tmp/init_script.influxql
-
-        echo "=> Executing the influxql script..."
-        influx -host=${INFLUX_HOST} -port=${INFLUX_API_PORT} -username=${ADMIN} -password="${PASS}" -import -path /tmp/init_script.influxql
-
-        echo "=> Influxql script executed."
-        touch "/data/.init_script_executed"
-    else
-        echo "=> No initialization script need to be executed"
-    fi
-
-    echo "=> Stopping InfluxDB ..."
-    if ! kill -s TERM %1 || ! wait %1; then
-        echo >&2 'InfluxDB init process failed.'
-        exit 1
-    fi
-fi
-
-echo "=> Starting InfluxDB in foreground ..."
-if [ -n "${JOIN}" ]; then
-    exec influxd -config=${CONFIG_FILE} -join ${JOIN}
 else
-    exec influxd -config=${CONFIG_FILE}
+    echo "=> No database need to be pre-created"
 fi
+
+fg
